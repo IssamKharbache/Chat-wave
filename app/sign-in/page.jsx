d495120bf0055f41c5c99d3b9941fd22014d5732
lefline <issamkharbache2@gmail.com>
2024-09-15 11:28:44 +0100

    creating sign in page

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,16 +1,42 @@
 "use client";
+import Logo from "@/components/global/Logo";
+import styles from "./signIn.module.css";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 
+//sign in with google function
 const SignInPage = () => {
+  const handleGoogleLogin = () => {};
+
   return (
-    <div>
-      <button
-        onClick={() => {
-          signIn("google");
-        }}
-      >
-        Sign in
-      </button>
+    <div className={styles.container}>
+      <Logo />
+      <form className={styles.form}>
+        <h1>Sign In</h1>
+        <input type="email" className={styles.input} placeholder="Email" />
+        <input
+          type="password"
+          className={styles.input}
+          placeholder="Password"
+        />
+        <button className={styles.button}>Sign in</button>
+
+        <p className={styles.or}>-OR-</p>
+        <button
+          onClick={() => signIn("google")}
+          className={styles.buttonGoogle}
+        >
+          <span>Continue with Google</span>
+          <FcGoogle />
+        </button>
+        <p className={styles.signIn}>
+          Don't have an account ?
+          <Link className={styles.link} href="/sign-up">
+            Sign up
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,11 +1,13 @@
 import Logo from "@/components/global/Logo";
 import styles from "./singUp.module.css";
+import Link from "next/link";
 
 const SignUpPage = () => {
   return (
     <div className={styles.container}>
+      <Logo />
       <form className={styles.form}>
-        <Logo />
+        <h1>Sign up</h1>
         <input type="text" className={styles.input} placeholder="Username" />
         <input type="email" className={styles.input} placeholder="Email" />
         <input
@@ -14,6 +16,12 @@ const SignUpPage = () => {
           placeholder="Password"
         />
         <button className={styles.button}>Sign Up</button>
+        <p className={styles.signIn}>
+          Already have an account ?{" "}
+          <Link className={styles.link} href="/sign-in">
+            Sign in
+          </Link>
+        </p>
       </form>
     </div>
   );
